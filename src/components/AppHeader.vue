c8ddbf5627e0fb516f76ec2896515a20d3d7890b
nicomanunta <nic.manun@gmail.com>
2024-01-10 15:51:44 +0100

    aggiunto titiolo e logo

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+<template lang="">
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mt-3">
+                    <img src="/public/ygo-logo.png" alt="">
+                    <h1>Yu-Gi-Oh Api</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/generals.scss" as *;
+
+
+h1 {
+    display: inline;
+    vertical-align: middle;
+    margin: 0px 20px;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppMain"
+}
+</script>
+<template lang="">
+    <main>
+        
+    </main>
+</template>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/generals.scss" as *;
+
+
+main {
+    background-color: $colore-primario;
+
+}
+</style>
